f6a16c4cc12fe586d9818a3fbc8bd4a98b548b1b
Christophe Boucharlat <garfieldounet+gitbrikabrok@gmail.com>
2016-01-14 12:31:08 +0100

    changed things

--- dockerpuller/app.py
+++ dockerpuller/app.py
@@ -28,11 +28,11 @@ def hook_listen():
                     try:
 #                        subprocess.call([hook_value, pretty_request])
 #                        subprocess.call([hook_value, pretty_request])
-                        subprocess.call(['scripts/testing.sh',
+                        subprocess.call(['scripts/logging.sh',
                             '--push_date', '{data}'.format(data=data['push_data']['pushed_at']),
                             '--tag', '{data}'.format(data=data['push_data']['tag']),
                             '--repo_name', '{data}'.format(data=data['repository']['repo_name'])])
-                        subprocess.call([hook_value, data])
+                        subprocess.call([hook_value, '{data}'.format(data=data)])
                         return jsonify(success=True), 200
                     except OSError as e:
                         return jsonify(success=False, error=str(e)), 400
