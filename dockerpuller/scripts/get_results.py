14d0e323fc861d715ff449acc23667aa669e5d18
Christophe Boucharlat <garfieldounet+gitbrikabrok@gmail.com>
2016-01-15 19:02:56 +0100

    working creation of service

--- dockerpuller/scripts/get_results.py
+++ dockerpuller/scripts/get_results.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import json, ast
-from pprint import pprint
+import json, re
 
 '''
 { service_name: "webhook-tester-repo@1", docker_image: "biscarch/webhook-tester-repo", version: "toto"}
@@ -11,14 +10,40 @@ from pprint import pprint
 store = []
 def main():
     store_file="logs/testing.log"
+    version_patern = re.compile("^\d+\.\d+\.\d+$")
+    services_list = ['xorilog/docker-webhook', 'surycat/wings-appreminder']
+    final_services_list = []
     with open(store_file, 'r') as f_in:
         for line in f_in:
             data = json.loads(line)
             store.append(data)
-    for item in store:
-        docker_image=item['repository']['repo_name']
-        version=item['push_data']['tag']
-        print "{ service_name: \"" + str(docker_image).split("/")[1] + "@1\", docker_image: \"" + str(docker_image) + "\", version: \"" + str(version) + "\"}"
+#    for item in store:
+#        docker_image=item['repository']['repo_name']
+#        version=item['push_data']['tag']
+#        print "{ service_name: \"" + str(docker_image).split("/")[1] + "@1\", docker_image: \"" + str(docker_image) + "\", version: \"" + str(version) + "\"}"
+    temp_dict = dict()
+    for i in xrange(len(store)):
+        if store[i]['repository']['repo_name'] not in temp_dict:
+            temp_dict[store[i]['repository']['repo_name']]=[]
+        if store[i]['push_data']['tag'] not in temp_dict[store[i]['repository']['repo_name']]:
+            if version_patern.match(store[i]['push_data']['tag']):
+                temp_dict[store[i]['repository']['repo_name']].append(store[i]['push_data']['tag'])
+    
+    print temp_dict
+    print "Listing Max services"
+    for service in services_list:
+        final_services_list.append([service, max(temp_dict[service])])
+    print final_services_list
+    print "Surycat configuration for install-machine :"
+    for f in final_services_list:
+        for item in store:
+            if (f[0] in item['repository']['repo_name']) and (f[1] in item['push_data']['tag']) :
+                docker_image=item['repository']['repo_name']
+                version=item['push_data']['tag']
+                print "{ service_name: \"" + str(docker_image).split("/")[1] + "@1\", docker_image: \"" + str(docker_image) + "\", version: \"" + str(version) + "\"}"
+#    for item in temp_list:
+#        for i in xrange(len(store)):
+#            if store[i]
         
 
 if __name__ == "__main__":
