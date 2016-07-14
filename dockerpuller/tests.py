a5b8f3da7fd2413b38a7057921a63bd753b1f41c
Brenton Bills <fud.fud@gmail.com>
2016-07-15 07:43:38 +1000

    Fixing up unit tests for newer url structure

--- dockerpuller/tests.py
+++ dockerpuller/tests.py
@@ -1,6 +1,8 @@
-import unittest
-from mock import patch
 import app
+import unittest
+
+from mock import patch
+
 
 class DockerPullerTestCase(unittest.TestCase):
 
@@ -16,30 +18,31 @@ class DockerPullerTestCase(unittest.TestCase):
             }
         app.config = config_dict
 
+    def test_get_api_version(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        assert 'Version' in response.data
+        
     @patch('subprocess.call')
     def test_valid_token_and_hook(self, subprocess_call):
-        response = self.app.post("/?token=abc123&hook=hello")
+        response = self.app.post("/abc123/hello", environ_base={"REMOTE_ADDR": "127.0.0.1"})
         self.assertEqual(response.status_code, 200)
-        subprocess_call.assert_called_once_with("scripts/hello.sh")
+        subprocess_call.assert_called_once_with(["scripts/hello.sh", "127.0.0.1"])
 
     def test_invalid_token(self):
-        response = self.app.post("/?token=abc123456&hook=hello")
-        self.assertEqual(response.status_code, 400)
+        response = self.app.post("/abc123456/hello")
+        self.assertEqual(response.status_code, 403)
         assert "Invalid token" in response.data
 
-    def test_get_not_allowed(self):
-        response = self.app.get("/?token=abc123&hook=hello")
-        self.assertEqual(response.status_code, 405)
-
     def test_invalid_hook(self):
-        response = self.app.post("/?token=abc123&hook=wronghook")
+        response = self.app.post("/abc123/wronghook")
         self.assertEqual(response.status_code, 404)
         assert "Hook not found" in response.data
 
     def test_hook_not_specified(self):
-        response = self.app.post("/?token=abc123")
-        self.assertEqual(response.status_code, 400)
-        assert "missing hook" in response.data
+        response = self.app.post("/abc123")
+        self.assertEqual(response.status_code, 404)
+
 
 if __name__ == '__main__':
     unittest.main()
